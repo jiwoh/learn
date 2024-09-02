b440f86daaca556e5fd27c81666b1c28175593f1
masterthepixel <45719243+masterthepixel@users.noreply.github.com>
2024-09-01 20:05:58 -0400

    Updates
    Text balance update
    upgrade tailwindcss

--- src/components/HomeVideoHero.jsx
+++ src/components/HomeVideoHero.jsx
@@ -2,41 +2,68 @@ import React, { useEffect } from 'react'
 import { Container } from './Container'
 import Link from 'next/link'
 import AOS from 'aos'
+
 export function HomeVideoHero() {
   useEffect(() => {
+    // Initialize AOS
     AOS.init({
       duration: 1000, // Animation Duration
       easing: 'ease-in-out', // Animation Transition
     })
+
+    // Ensure the video plays automatically
+    const videoElement = document.querySelector('video')
+    if (videoElement) {
+      videoElement.play().catch((error) => {
+        // Autoplay was prevented
+        console.log('Autoplay prevented:', error)
+      })
+    }
   }, [])
+
   return (
     <section className="relative flex flex-col items-center justify-center h-screen -mt-24 text-center text-white">
-      <div data-aos="fade-up" className="absolute top-0 left-0 w-full h-full overflow-hidden ">
+      <div
+        data-aos="fade-up"
+        className="absolute top-0 left-0 w-full h-full overflow-hidden"
+      >
         <video
-          className="absolute object-cover min-w-full min-h-full "
+          className="absolute object-cover min-w-full min-h-full"
           src="/videos/laptopuser.mp4"
           type="video/mp4"
-          codecs="avc1.42E01E, mp4a.40.2"
           autoPlay
           muted
           loop
-        ></video>
+          playsInline
+        >
+          {/* Fallback content in case the video doesn't play */}
+          <img src="/learncyba_ring.webp" alt="Fallback content" />
+        </video>
       </div>
       <div className="absolute top-0 left-0 z-0 w-full h-full bg-neutral-950/75"></div>
 
-      <div className="... z-10 mx-auto -mt-42 max-w-4xl space-y-4  px-2 py-2 text-center sm:py-8 md:space-x-6 md:px-0 lg:-mt-32 lg:space-y-8 lg:py-6 ">
-        <h1 data-aos="fade-up" className="mt-4 text-4xl sm:text-[min(14vw,4.2rem)] font-bold leading-none tracking-tight">
-          <span className="... bg-gradient-to-br from-blue-100 via-slate-200 to-amber-100 bg-clip-text text-transparent ">
+      <div className="z-10 max-w-4xl px-2 py-2 mx-auto space-y-4 text-center -mt-42 sm:py-8 md:space-x-6 md:px-0 lg:-mt-32 lg:space-y-8 lg:py-6">
+        <h1
+          data-aos="fade-up"
+          className="mt-4 text-balance text-4xl font-bold leading-none tracking-tight sm:text-[min(14vw,4.2rem)]"
+        >
+          <span className="text-transparent bg-gradient-to-br from-blue-100 via-slate-200 to-amber-100 bg-clip-text">
             Level up your tech career to 6-figures and beyond with
             CyberSecurity.
           </span>
         </h1>
-        <p data-aos="fade-down" className="max-w-4xl py-6 mx-auto text-lg leading-normal text-center md:text-xl sm:px-6 text-zinc-200 md:px-12">
+        <p
+          data-aos="fade-down"
+          className="max-w-4xl py-6 mx-auto text-lg leading-normal text-center text-zinc-200 sm:px-6 md:px-12 md:text-xl"
+        >
           Start your new career as a Cybersecurity Compliance Specialist and
-          earn 6-figures in about 6 months. There are no certifications, degrees, or coding
-          required.
+          earn 6-figures in about 6 months. There are no certifications,
+          degrees, or coding required.
         </p>
-        <div data-aos="fade-up" className="z-10 flex items-center justify-center mt-1 opacity-100 gap-x-6">
+        <div
+          data-aos="fade-up"
+          className="z-10 flex items-center justify-center mt-1 opacity-100 gap-x-6"
+        >
           <Link
             href="https://calendly.com/d/4sq-mpj-ktz/cybersecurity-career-advice-strategy-session"
             className="rounded-md bg-blue-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
@@ -48,7 +75,7 @@ export function HomeVideoHero() {
             href="#course-structure"
             className="flex items-center justify-center px-8 py-3 text-xs text-center text-white transition-all border rounded-md whitespace-nowrap border-zinc-700 bg-zinc-900/20 backdrop-blur hover:bg-emerald-800 sm:text-sm"
           >
-            Learn more{' '}
+            Learn more
           </Link>
         </div>
       </div>
